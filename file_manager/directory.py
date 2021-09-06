f7e7153e556d6c6603d7e941e183c64e056f3d3d
Peter Laraia <pvlaraia@gmail.com>
2021-09-06 10:25:47 -0700

    # TItle: set it up so entire path can live in Fiji.app/scripts/AppName
    # Summary: I dont think I'm gonna stick with this, but it's nice for now cause it lets you run it directly from the Fiji toolbar.
    I think what i'll do down the line though is create this as a package to stick in Fiji.app/jars/Lib, then have a separate file in scripts to run it?

--- file_manager/directory.py
+++ file_manager/directory.py
@@ -3,8 +3,5 @@ from ij import IJ, ImagePlus
 
 class IJDirectory:
     def __init__(self, label):
-        self.path = None
         self.label = label
-
-    def initialize(self):
         self.path = IJ.getDirectory('{} Directory'.format(self.label))
--- imaging_.py
+++ imaging_.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import inspect
+from ij import IJ;
 
 '''
 Add current path to sys path so that python knows it can look for modules
@@ -8,10 +9,12 @@ Add current path to sys path so that python knows it can look for modules
 '''
 current_filename = inspect.getframeinfo(inspect.currentframe()).filename
 current_path = os.path.dirname(os.path.abspath(current_filename))
+# If running directly from Fiji plugin bar, it gets the path wrong because it leaves out the scripts folder, idk why
+sys.path.append(os.getcwd() + '\scripts' + current_path.split(os.getcwd())[1])
 sys.path.append(current_path)
+for path in sys.path:
+    IJ.log(path)
 
-
-from ij import IJ;
 from file_manager.directory import IJDirectory
 
 def run():
